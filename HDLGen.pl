8a25589ccca40726d8f1fd9353ac00b9c542e9f9
WilsonChen003 <chen.shengwei@outlook.com>
2023-05-07 22:02:49 +0800

    add interface filter for a new issue

--- HDLGen.pl
+++ HDLGen.pl
@@ -87,7 +87,7 @@ sub Version {
 	print BOLD BLUE <<EOF;
 
          *********************************************
-         ****** Current HDLGen Version is V1.16 ******
+         ****** Current HDLGen Version is V1.17 ******
          *********************************************
 EOF
 
